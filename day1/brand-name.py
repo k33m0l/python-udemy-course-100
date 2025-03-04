a852c39d8f2d0a3bf0c666215bacea60b0caab89
k33m0l <amolnar4d41@gmail.com>
2025-03-04 18:33:29 +0100

    Day 1 completed

---
+++ day1/brand-name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator.")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+print(f"Your brand name could be {city} {pet}.")
