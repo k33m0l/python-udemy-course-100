743025d7816fff4c579bcf97d28d6918bf1677fc
k33m0l <amolnar4d41@gmail.com>
2025-03-31 19:13:06 +0200

    day8

--- day8/age.py
+++ day8/age.py
@@ -5,5 +5,3 @@ def life_in_weeks(age):
     remaining_years = max_age - age
     remaining_weeks = remaining_years * weeks_in_year
     return f"You have {remaining_weeks} weeks left."
-
-print(life_in_weeks(56))
