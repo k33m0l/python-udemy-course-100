940f18dd4f30248938ead9b87aa8d56bb321db96
k33m0l <k33m0l@users.noreply.github.com>
2025-04-06 11:48:18 +0200

    day23

--- day23/main.py
+++ day23/main.py
@@ -5,10 +5,34 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=600, height=600, startx=0, starty=0)
 screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move, "space")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    # crossing check
+    if player.is_finish_crossed():
+        scoreboard.update_level()
+        car_manager.level_up()
+        player.reset_pos()
+    
+screen.exitonclick()
