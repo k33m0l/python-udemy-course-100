217a5f7d5d91000d6502d492fb3e4cb5c509bcce
k33m0l <k33m0l@users.noreply.github.com>
2025-04-10 22:56:46 +0200

    day28

--- day28/main.py
+++ day28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -6,15 +7,55 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
+CHECKMARK = "✓"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+SECONDS_IN_A_MIN = 60
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer", fg=GREEN)
+    checkmarks.config(text="")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    if reps % 8 == 0:
+        reps = 0
+        timer_label.config(text="Break", fg=RED)
+        count_down(LONG_BREAK_MIN * SECONDS_IN_A_MIN)
+    elif reps % 2 == 0:
+        timer_label.config(text="Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN * SECONDS_IN_A_MIN)
+    else:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(WORK_MIN * SECONDS_IN_A_MIN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    minutes = math.floor(count / SECONDS_IN_A_MIN)
+    if minutes < 10:
+        minutes = f"0{minutes}"
+    seconds = count % SECONDS_IN_A_MIN
+    if seconds < 10:
+        seconds = f"0{seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        for _ in range(math.floor(reps/2)):
+            mark += CHECKMARK 
+            checkmarks.config(text=mark)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -24,7 +65,17 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 background_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=background_image)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+timer_text = canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+checkmarks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+start_button = Button(text="Start", command=start_timer, highlightthickness=0)
+reset_button = Button(text="Reset", command=reset_timer, highlightthickness=0)
+
+timer_label.grid(row=0, column=1)
+canvas.grid(row=1, column=1)
+start_button.grid(row=2, column=0)
+reset_button.grid(row=2, column=2)
+checkmarks.grid(row=3, column=1)
 
 window.mainloop()
