87b28d8665c04e49d1c70eb1a9a534b285a5a31e
k33m0l <k33m0l@users.noreply.github.com>
2025-04-12 10:14:59 +0200

    day30

--- day26/nato/main.py
+++ day26/nato/main.py
@@ -5,4 +5,14 @@ import pandas
 nato = {row.letter:row.code for (_, row) in pandas.read_csv("nato_phonetic_alphabet.csv").iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-print([nato[c] for c in input("Provide a word: ").upper()])
+def getUserInput():
+    return input("Provide a word: ").upper()
+
+def getNatoWord():
+    try:
+        return [nato[c] for c in getUserInput()]
+    except KeyError:
+        print("Sorry only letters in the ABC please.")
+        return getNatoWord()
+
+print(getNatoWord())
