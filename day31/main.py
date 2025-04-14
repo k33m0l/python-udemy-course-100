cc9364a8bc8e41c817b50922e87484128b3f62ec
k33m0l <k33m0l@users.noreply.github.com>
2025-04-14 17:23:17 +0200

    day31

--- day31/main.py
+++ day31/main.py
@@ -8,13 +8,32 @@ from tkinter import *
 import pandas
 import random
 
-dictionary = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+try:
+    dictionary = pandas.read_csv("data/words_to_learn.csv").to_dict(orient="records")
+except FileNotFoundError:
+    print("Saved file not found, falling back to default list")
+    dictionary = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
 current_card = {}
 
 def flip_card():
     canvas.itemconfig(card_bg, image=card_back)
     canvas.itemconfig(language_text, text=ENGLISH_LANGUAGE_KEY)
     canvas.itemconfig(word_text, text=current_card[ENGLISH_LANGUAGE_KEY])
+
+def missed_word():
+    next_word()
+
+def save_dict():
+    words_to_learn = pandas.DataFrame.from_dict(dictionary)
+    words_to_learn.to_csv("data/words_to_learn.csv")
+
+def knew_word():
+    try:
+        dictionary.remove(current_card)
+        save_dict()
+    except ValueError:
+        window.destroy()
+    next_word()
 
 def next_word():
     global current_card, flip_timer
@@ -39,10 +58,10 @@ language_text = canvas.create_text(400, 150, text="Language", font=LANGUAGE_FONT
 word_text = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
 
 accept_image = PhotoImage(file="images/right.png")
-accept_button = Button(image=accept_image, highlightthickness=0, command=next_word)
+accept_button = Button(image=accept_image, highlightthickness=0, command=knew_word)
 accept_button.grid(row=1, column=1)
 deny_image = PhotoImage(file="images/wrong.png")
-deny_button = Button(image=deny_image, highlightthickness=0)
+deny_button = Button(image=deny_image, highlightthickness=0, command=missed_word)
 deny_button.grid(row=1, column=0)
 
 # Init a first card to show
