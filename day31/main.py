81f3d0bbe55c6bf483297f94bd1cdcc581ef21ae
k33m0l <k33m0l@users.noreply.github.com>
2025-04-14 16:49:07 +0200

    day31 next word

--- day31/main.py
+++ day31/main.py
@@ -1,8 +1,19 @@
 BACKGROUND_COLOR = "#B1DDC6"
 LANGUAGE_FONT = ("Ariel", 40, "italic")
 WORD_FONT = ("Ariel", 60, "bold")
+FRENCH_LANGUAGE_KEY = "French"
+ENGLISH_LANGUAGE_KEY = "English"
 
 from tkinter import *
+import pandas
+import random
+
+dictionary = pandas.read_csv("data/french_words.csv").to_dict(orient="records")
+
+def next_word():
+    current = random.choice(dictionary)
+    canvas.itemconfig(language_text, text=FRENCH_LANGUAGE_KEY)
+    canvas.itemconfig(word_text, text=current[FRENCH_LANGUAGE_KEY])
 
 window = Tk()
 window.title("Flashcard App")
@@ -13,14 +24,14 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 canvas.create_image(400, 263, image=card_front)
 canvas.grid(row=0, column=0, columnspan=2)
 
-canvas.create_text(400, 150, text="Language", font=LANGUAGE_FONT)
-canvas.create_text(400, 263, text="Word", font=WORD_FONT)
+language_text = canvas.create_text(400, 150, text="Language", font=LANGUAGE_FONT)
+word_text = canvas.create_text(400, 263, text="Word", font=WORD_FONT)
 
 accept_image = PhotoImage(file="images/right.png")
-accept_button = Button(image=accept_image, highlightthickness=0)
-accept_button.grid(row=1, column=0)
+accept_button = Button(image=accept_image, highlightthickness=0, command=next_word)
+accept_button.grid(row=1, column=1)
 deny_image = PhotoImage(file="images/wrong.png")
 deny_button = Button(image=deny_image, highlightthickness=0)
-deny_button.grid(row=1, column=1)
+deny_button.grid(row=1, column=0)
 
 window.mainloop()
