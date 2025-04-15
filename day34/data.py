82924fdd214299a5bb3a7d11614e26db64cd498a
k33m0l <k33m0l@users.noreply.github.com>
2025-04-15 19:55:01 +0200

    day34 refactor

--- day34/data.py
+++ day34/data.py
@@ -1,6 +1,12 @@
 import requests
 
-response = requests.get(url="https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=boolean")
+parameters = {
+    "amount": 3,
+    "type": "boolean",
+    "category": 18
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 data = response.json()
 question_data = []
