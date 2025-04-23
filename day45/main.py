1db86da34436313017adb9bb79bf87e5e036ac7f
k33m0l <k33m0l@users.noreply.github.com>
2025-04-23 15:38:57 +0200

    day45

--- day45/main.py
+++ day45/main.py
@@ -5,4 +5,10 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(url=URL)
+soup = BeautifulSoup(response.text, "html.parser")
 
+titles = [title_tag.get_text() for title_tag in soup.find_all(name="h3", class_="title")]
+titles.reverse()
+for title in titles:
+    print(f"{title}")
