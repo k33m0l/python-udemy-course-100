d4f2468316fe2d06cf6dfb0bbb3f13b137654641
k33m0l <k33m0l@users.noreply.github.com>
2025-04-25 22:05:37 +0200

    day48

--- day48/scrape.py
+++ day48/scrape.py
@@ -2,9 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
-driver.get("https://google.com")
+driver.get("https://www.python.org/")
 
-text = driver.find_element(By.CLASS_NAME, "RNmpXc").get_attribute("value")
-print(text)
+elements = driver.find_elements(By.CSS_SELECTOR, ".event-widget div ul li")
+
+events = {}
+for index in range(0, len(elements)):
+    event_title = elements[index].find_element(By.TAG_NAME, "a").text
+    event_date = elements[index].find_element(By.TAG_NAME, "time").get_attribute("datetime").split("T")[0]
+    events[index] = {
+        "time": event_date,
+        "name": event_title
+    }
+
+print(events)
 
 driver.quit()
