cfb6439d3a3683320814be4fe8906495959fd3df
k33m0l <k33m0l@users.noreply.github.com>
2025-04-26 22:57:33 +0200

    day54

--- day54/tutorial/main.py
+++ day54/tutorial/main.py
@@ -1,7 +1,26 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
+def logger_decorator(func):
+    def wrapper():
+        print("Decorator called")
+        func()
+    return wrapper
+
+
+@logger_decorator
+def random_function():
+    print("Function called")
 
 
 @app.route("/")
 def hello_world():
+    random_function()
     return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run()
+
