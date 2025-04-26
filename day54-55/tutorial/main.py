e1ed4da7a4465033995f205c98d71ebe293be6f6
k33m0l <k33m0l@users.noreply.github.com>
2025-04-26 23:56:59 +0200

    day55

--- day54-55/tutorial/main.py
+++ day54-55/tutorial/main.py
@@ -1,24 +1,43 @@
 from flask import Flask
+import time
 
 app = Flask(__name__)
 
 
-def logger_decorator(func):
-    def wrapper():
-        print("Decorator called")
-        func()
+def adv_logger(func):
+    def wrapper(*args):
+        print(f"You called {func.__name__}{args}")
+        result = func(*args)
+        print(f"It returned: {result}")
+        return result
     return wrapper
 
 
-@logger_decorator
-def random_function():
-    print("Function called")
+def timer(func):
+    def wrapper():
+        before_time = time.time()
+        result = func()
+        after_time = time.time()
+        print(f"{func.__name__} run speed: {after_time - before_time}s")
+        return result
+    return wrapper
+
+
+def title(func):
+    def wrapper():
+        return f"<h1>{func()}</h1>"
+    return wrapper
+
+
+@timer
+@title
+def content():
+    return "Hello World"
 
 
 @app.route("/")
 def hello_world():
-    random_function()
-    return "Hello World"
+    return content()
 
 
 if __name__ == "__main__":
