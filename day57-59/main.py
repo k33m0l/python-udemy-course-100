6f678b1c7a645e3d72ba5c9151c21621bca6b587
k33m0l <k33m0l@users.noreply.github.com>
2025-04-27 20:51:22 +0200

    day57

--- day57-59/main.py
+++ day57-59/main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import requests
 
-
+URL="https://api.npoint.io/c790b4d5cab58020d391"
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get(url=URL)
+    response = response.json()
+    return render_template("index.html", posts=response, is_main=True)
+
+@app.route("/blog/<int:post_num>")
+def blog(post_num: int):
+    response = requests.get(url=URL)
+    response = response.json()
+    return render_template("index.html", posts=[blog for blog in response if blog["id"] == post_num], is_main=False)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
