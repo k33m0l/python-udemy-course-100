d9da0f42f1ffdc82655d32767b98fd3d418c5074
k33m0l <k33m0l@users.noreply.github.com>
2025-04-27 22:15:50 +0200

    day59

--- day57-59/main.py
+++ day57-59/main.py
@@ -16,6 +16,14 @@ def blog(post_num: int):
     response = response.json()
     return render_template("index.html", posts=[blog for blog in response if blog["id"] == post_num], is_main=False)
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run()
 
