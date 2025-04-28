f214e52c55683c0507889df32195964d7a1c4873
k33m0l <k33m0l@users.noreply.github.com>
2025-04-28 14:42:13 +0200

    day61

--- day61/main.py
+++ day61/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 '''
 Red underlines? Install the required packages first: 
@@ -12,15 +15,28 @@ pip3 install -r requirements.txt
 
 This will install the packages from requirements.txt for this project.
 '''
-
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField("login")
 
 app = Flask(__name__)
-
+app.secret_key = "verystrong"
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template("login.html", form=login_form)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
