006441b2b5b2c1f5db579df7e3e2cffe4ea81c9f
k33m0l <k33m0l@users.noreply.github.com>
2025-05-10 15:19:53 +0200

    day74

--- day74/main.py
+++ day74/main.py
@@ -11,9 +11,20 @@ print(f"LEGO released {sorted_sets[sorted_sets["year"] == 1949]} sets in their f
 print(f"LEGO sets with the most number of parts are: {sorted_sets.sort_values("num_parts", ascending=False).head()}")
 
 sets_per_year = sets.groupby("year").count()
-# pyplot.plot(sets_per_year.index[:-2], sets_per_year.set_num[:-2])
-# pyplot.show()
-
 themes_by_year = sets.groupby("year").agg({"theme_id": pandas.Series.nunique})
-pyplot.plot(themes_by_year.index[:-2], themes_by_year.theme_id[:-2])
+parts_per_set = sets.groupby("year").agg({"num_parts": pandas.Series.mean})
+
+axis1 = pyplot.gca()
+axis2 = axis1.twinx()
+
+axis1.plot(sets_per_year.index[:-2], sets_per_year.set_num[:-2], color="g")
+axis2.plot(themes_by_year.index[:-2], themes_by_year.theme_id[:-2], color="b")
+
+axis1.set_xlabel("Year")
+axis1.set_ylabel("Number of Sets", color="green")
+axis2.set_ylabel("Number of Themes", color="blue")
+
+pyplot.scatter(parts_per_set.index[:-2], parts_per_set.num_parts[:-2])
+
 pyplot.show()
+
